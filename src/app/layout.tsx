3dcd491ef7e7075161f176f99ffc3bdb7150b34b
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2024-11-04 18:08:54 -0300

    add carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,23 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link"
 import localFont from "next/font/local";
-import { ChevronDown } from "lucide-react";
+import { Menu } from "lucide-react";
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuLink,
-  navigationMenuTriggerStyle,
   NavigationMenuTrigger,
   NavigationMenuItem,
   NavigationMenuList
 } from "@/components/ui/navigation-menu"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -41,32 +47,50 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="bg-gray-950 flex flex-col">
-          <div className="h-24">
-          </div>
+          <div className="h-26 flex flex-row justify-center gap-4">
+            <Image
+              src={"/logoMR.png"}
+              width={250}
+              height={250}
+              alt="Logo Maua Racing"
+            />
+            <div className="place-content-center">
+              <div className="md:hidden">
+                <Sheet>
+                  <SheetTrigger className="bg-red-300"><Menu/></SheetTrigger>
+                  <SheetContent className="bg-gray-950 text-white">
+                    <SheetHeader className="">
+                      <SheetTitle className="border-b text-center text-inherit">
+                        <Link href={"/"}>Maua Racing</Link>
+                        </SheetTitle>
 
-          {/* <Image
-            src={"/public/logoMR.jpeg"}
-            width={500}
-            height={500}
-            alt="Logo Maua Racing"
-          /> */}
-          <div className="h-2 bg-red-500 border-t"></div>
-          <div className="justify-center place-items-center">
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-white">Sobre</NavigationMenuTrigger>
-                  <NavigationMenuContent></NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-white">Nossa história</NavigationMenuTrigger>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-white">Contato</NavigationMenuTrigger>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
+                      <SheetDescription>
+                    
+                      </SheetDescription>
+                      
+                    </SheetHeader>
+                  </SheetContent>
+                </Sheet>
+              </div>
+              <NavigationMenu className="hidden md:inline-flex">
+                <NavigationMenuList>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="bg-inherit text-white hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-white font-semibold">Sobre</NavigationMenuTrigger>
+                    <NavigationMenuContent></NavigationMenuContent>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="bg-inherit text-white hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-white font-semibold">Nossa história</NavigationMenuTrigger>
+                    <NavigationMenuContent></NavigationMenuContent>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger className="bg-inherit text-white hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-white font-semibold">Contato</NavigationMenuTrigger>
+                    <NavigationMenuContent></NavigationMenuContent>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
           </div>
+          <div className="h-2 bg-red-500 border-t" />
         </header>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Images } from "@/ui/images";
+
+
 
 export default function Home() {
   return (
-    <div></div>
+    <Images/>
   );
 }
