99501d4184fe3d7e2be9da486b3bdb01607429ae
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2024-11-07 21:06:21 -0300

    fix navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
 import Image from "next/image";
 import bg from "../../public/images/79.jpg"
-import Link from "next/link"
-import { Menu } from "lucide-react";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuTrigger,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+import Link from "next/link";
+import { ChevronDown } from "lucide-react";
+
 
 
 export default function Home() {
   return (
-    <section className="flex justify-center overflow-y-hidden">
-      <Image className="-z-10 -mt-28" src={"/images/79.jpg"} width={bg.width} height={bg.width} alt="" sizes={""} />
-      <div className="bg-transparent absolute flex flex-col top-12">
+    <section className={`flex justify-center`}>
+      <Image className="-z-10 object-cover" src={bg} fill alt=""/>
+      <div className="bg-transparent absolute flex flex-col top-12 lg:top-0">
         <div className="h-26 flex flex-col md:justify-between gap-4 my-2 justify-center">
           <div className="flex justify-center">
             <Image
@@ -34,90 +18,44 @@ export default function Home() {
               height={210}
               alt="Logo Maua Racing"
               className="ml-3"
+              priority={true}
 
             />
           </div>
           <div className="place-content-center">
-            <div className="md:hidden">
-              <Sheet>
-                <SheetTrigger className="bg-red-300"><Menu /></SheetTrigger>
-                <SheetContent className="bg-gray-950 text-gray-950">
-                  <SheetHeader className="">
-                    <SheetTitle className="border-b text-center text-inherit">
-                      <Link href={"/"}>Maua Racing</Link>
-                    </SheetTitle>
-
-                    <SheetDescription>
-
-                    </SheetDescription>
-
-                  </SheetHeader>
-                </SheetContent>
-              </Sheet>
-            </div>
-            <NavigationMenu className="hidden md:inline-flex">
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg">QUEM SOMOS</NavigationMenuTrigger>
-                  <NavigationMenuContent className="">
-                    <Link href={""} legacyBehavior passHref>
-                      <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                        O que é FSAE?
-                      </NavigationMenuLink>
-                    </Link>
-                    <Link href={""} legacyBehavior passHref>
-                      <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                        Nossa equipe
-                      </NavigationMenuLink>
-                    </Link>
-                    <Link href={""} legacyBehavior passHref>
-                      <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                        Patrocinadores
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg">CARROS</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href={""} legacyBehavior passHref>
-                    <NavigationMenuLink className={`bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg px-2`}>
-                      GALERIA
-                    </NavigationMenuLink>
+            <nav className="">
+              <div className="flex md:flex-row flex-col place-content-center">
+                <div className="group">
+                  <Link href={"/"} className="px-4 flex flex-row font-semibold text-lg hover:border-t-2 border-red-500 transition-[border] duration-75 ease-out">
+                    QUEM SOMOS
+                    <ChevronDown className="ml-1 w-4"/>
                   </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href={""} legacyBehavior passHref>
-                    <NavigationMenuLink className={`bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg px-2`}>
-                      MÍDIA
-                    </NavigationMenuLink>
+                  <div className="hidden bg-white border rounded-md mt-2 group-hover:flex">
+                    <div className="flex flex-col justify-center grow pt-1 px-2">
+                      <Link href={"/"} className="border-b">O que é FSAE?</Link>
+                      <Link href={"/"} className="border-b ">Nossa equipe</Link>
+                      <Link href={"/"} className="border-b ">Patrocinadores</Link>
+                    </div>
+                  </div>
+                </div>
+                <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">CARROS</Link>
+                <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">GALERIA</Link>
+                <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">MÍDIA</Link>
+                <div className="group">
+                  <Link href={"/"} className="px-4 flex flex-row font-semibold text-lg hover:border-t-2 border-red-500 w-44">
+                    PARTICIPE
+                    <ChevronDown className="ml-1 w-4"/>
                   </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg">PARTICIPE</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <Link href={""} legacyBehavior passHref>
-                      <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                        Seja um patrocinador
-                      </NavigationMenuLink>
-                    </Link>
-                    <Link href={""} legacyBehavior passHref>
-                      <NavigationMenuLink className={`${navigationMenuTriggerStyle()}`}>
-                        Seja um membro
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger className="bg-inherit text-gray-950 hover:border-t-2 border-red-500 rounded-none hover:bg-inherit hover:text-gray-950 font-semibold text-lg">CONTATOS</NavigationMenuTrigger>
-                  <NavigationMenuContent></NavigationMenuContent>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
+                  <div className="hidden bg-white border rounded-md mt-2 group-hover:flex">
+                    <div className="flex flex-col justify-center grow pt-1 px-2 shrink">
+                      <Link href={"/"} className="border-b ">Seja um patrocinador</Link>
+                      <Link href={"/"} className="border-b ">Seja um membro</Link>
+                    </div>
+                  </div>
+                </div>
+                <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">CONTATO</Link>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
