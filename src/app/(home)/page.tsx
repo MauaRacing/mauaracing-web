d14818d311758f2996466533d1392791ea9a2952
Pedro Correia <pedro.tukinho@gmail.com>
2024-11-13 20:38:52 -0300

    add images to about page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,9 +7,9 @@ import { Footer } from "@/app/ui/footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <main className={`flex justify-center h-dvh`}>
-        <Image className="-z-10 object-cover" src={bg} fill alt="" />
+    <div className="flex flex-col relative">
+      <main className={`flex justify-center min-h-dvh lg:h-[130dvh]`}>
+        <Image className="-z-10 object-cover " src={bg} fill alt="" />
         <div className="bg-transparent flex flex-col mt-12 lg:mt-0">
           <div className="h-24 flex flex-col md:justify-between gap-4 my-2 justify-center">
             <div className="flex items-center justify-center">
@@ -18,14 +18,12 @@ export default function Home() {
                 src={"/logoMR_whitebg.png"}
                 width={210}
                 height={210}
-                alt="Logo Maua Racing"
+                alt="Logo Mauá Racing"
                 className="ml-3"
                 priority={true}
               />
             </div>
-            <div className="place-content-center">
-              <NavBar className="hidden md:block" />
-            </div>
+            <NavBar className="hidden md:block" />
           </div>
         </div>
       </main>
