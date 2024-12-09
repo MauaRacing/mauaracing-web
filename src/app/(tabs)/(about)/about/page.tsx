8afdd08ac402fedb56cdca22259102cc1d1ee8eb
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2024-12-09 15:28:11 -0300

    fix navbar

--- src/app/(tabs)/(about)/about/page.tsx
+++ src/app/(tabs)/(about)/about/page.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
+// import type { Metadata } from "next";
 import Image from "next/image";
 import image1 from "public/images/aboutImage.png"
 import image2 from "public/images/aboutImage2.png"
+
+// export const metadata: Metadata = {
+//   title: "Sobre",
+// };
 
 export default function Page() {
   return (
@@ -9,8 +14,8 @@ export default function Page() {
       <div className="w-[100%] mt-8">
         <article className="p-4 flex lg:flex-row flex-col items-center justify-center mb-5">
           <div className="flex flex-col justify-center items-center">
-            <h1 className="text-6xl my-2 text-center underline underline-offset-8 decoration-solid w-[100%]">Quem Somos</h1>
-            <p className="text-justify text-pretty indent-11 mx-10 text-2xl leading-relaxed w-[90%]">
+            <h1 className="text-6xl my-2 text-center underline underline-offset-8 decoration-solid ">Quem Somos</h1>
+            <p className="mt-5 text-justify text-pretty md:indent-11 mx-10 text-2xl leading-relaxed w-[90%]">
               Somos uma equipe de FSAE de alunos do Instituto Mauá de Tecnologia, que tem como objetivo o desenvolvimento de um projeto de carro tipo formula para competição.  A proposta consiste em desenvolver um projeto do zero, construí-lo, testá-lo e, enfim, levá-lo para competir. Embora a competição seja nosso objetivo final, todo o processo nos conduz ao aprendizado em diversas áreas, não só da engenharia e viabilidade do projeto, mas também de trabalho em grupo, organização, convivência, entre outros. Com a estrutura da Mauá, somos capazes de explorar ideias inovadoras na área e aprimorar o projeto a cada ano!
             </p>
           </div>
@@ -19,7 +24,7 @@ export default function Page() {
             width={600}
             height={600}
             alt="Logo Maua Racing"
-            className="ml-2 float-right mt-5 mr-5 object-none"
+            className="ml-2 mt-5 mr-5 lg:object-none"
           />
         </article>
       </div>
--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,63 +1,69 @@
+"use client"
 import Link from "next/link";
 import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
-export function NavBar({className, navMenu = false} : {className? : string, navMenu? : boolean}) {
+export function NavBar({ className, navMenu = false }: { className?: string, navMenu?: boolean }) {
+  const [openAbout, setOpenAbout] = useState(false);
+  const [openMedia, setOpenMedia] = useState(false);
+  const [openGetInvolved, setOpenGetInvolved] = useState(false);
+
   return (
     <nav className={className}>
-      <div className={`flex ${navMenu? "flex-col": "flex-row"} place-content-center`}>
-        <div className="group">
+      <div className={`flex ${navMenu ? "flex-col" : "flex-row"} place-content-center`}>
+        <div className="group relative" onMouseOver={() => setOpenAbout(true)} onMouseLeave={() => setOpenAbout(false)}>
           <Link href={"/about"} className="hidden px-4 md:flex md:flex-row font-semibold text-lg group-hover:border-t-2 border-red-500">
             QUEM SOMOS
-            <ChevronDown className="ml-1 w-4"/>
+            <ChevronDown className="ml-1 w-4" />
           </Link>
           <button className="md:hidden px-4 flex flex-row font-semibold text-lg group-hover:border-t-2 border-red-500">
             QUEM SOMOS
-            <ChevronDown className="ml-1 w-4"/>
+            <ChevronDown className="ml-1 w-4" />
           </button>
-          <div className="hidden bg-white border rounded-md group-focus-within:flex md:group-hover:flex lg:group-hover:absolute min-w-44">
+          <dialog open={openAbout} className=" bg-white border rounded-md min-w-44 md:absolute relative">
             <div className="flex flex-col justify-center grow pt-1 px-2 text-black">
               <Link href={"/about"} className="border-b md:hidden">Quem Somos</Link>
               <Link href={"/about-fsae"} className="border-b ">O que é FSAE?</Link>
               <Link href={"/our-team"} className="border-b ">Nossa equipe</Link>
               <Link href={"/sponsors"} className="border-b ">Patrocinadores</Link>
             </div>
-          </div>
+          </dialog>
         </div>
         <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500 ">CARROS</Link>
         <Link href={"/gallery"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">GALERIA</Link>
-        <div className="group">
+        <div className="group relative" onMouseOver={() => setOpenMedia(true)} onMouseLeave={() => setOpenMedia(false)}>
           <Link href={"/"} className="hidden px-4 md:flex md:flex-row font-semibold text-lg group-hover:border-t-2 border-red-500">
             MÍDIA
             <ChevronDown className="ml-1 w-4"/>
           </Link>
           <button className="md:hidden px-4 flex flex-row font-semibold text-lg group-hover:border-t-2 border-red-500 transition-[border] duration-75 ease-out">
             MÍDIA
-            <ChevronDown className="ml-1 w-4"/>
+            <ChevronDown className="ml-1 w-4" />
           </button>
-          <div className="hidden bg-white border rounded-md group-focus-within:flex md:group-hover:flex lg:group-hover:absolute">
-            <div className="flex flex-col justify-center grow pt-1 px-2 text-black">
+          <dialog open={openMedia} className="bg-white border rounded-md min-w-32 md:absolute relative">
+            <div className="flex flex-col justify-center grow pt-1 px-2 text-black text-nowrap">
               <Link href={"/"} className="border-b md:hidden">Mídia</Link>
               <Link href={"/"} className="border-b ">Notícias</Link>
               <Link href={"/"} className="border-b ">Vídeos</Link>
             </div>
-          </div>
+          </dialog>
         </div>
-        <div className="group">
+        <div className="group relative" onMouseOver={() => setOpenGetInvolved(true)} onMouseLeave={() => setOpenGetInvolved(false)}>
           <Link href={"/"} className="hidden px-4 md:flex md:flex-row font-semibold text-lg group-hover:border-t-2 border-red-500">
             PARTICIPE
             <ChevronDown className="ml-1 w-4"/>
           </Link>
-          <button className="md:hidden px-4 flex flex-row font-semibold text-lg group-hover:border-t-2 border-red-500 transition-[border] duration-75 ease-out">
+          <button  className="md:hidden px-4 flex flex-row font-semibold text-lg group-hover:border-t-2 border-red-500 transition-[border] duration-75 ease-out">
             PARTICIPE
-            <ChevronDown className="ml-1 w-4"/>
+            <ChevronDown className="ml-1 w-4" />
           </button>
-          <div className="hidden bg-white border rounded-md group-focus-within:flex md:group-hover:flex lg:group-hover:absolute">
-            <div className="flex flex-col justify-center grow pt-1 px-2 text-black">
+          <dialog open={openGetInvolved} className=" bg-white border rounded-md min-w-44 md:absolute relative">
+            <div className="flex flex-col justify-center grow pt-1 px-2 text-black text-nowrap">
               <Link href={"/"} className="border-b md:hidden">Participe</Link>
               <Link href={"/"} className="border-b ">Seja um patrocinador</Link>
               <Link href={"/"} className="border-b ">Seja um membro</Link>
             </div>
-          </div>
+          </dialog>
         </div>
         <Link href={"/"} className="px-4 font-semibold text-lg hover:border-t-2 border-red-500">CONTATO</Link>
       </div>
