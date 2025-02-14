dd54bfb2e973f27b956a056d9346fc384fe1bb95
rogeriocassares <roger10cassares@gmail.com>
2025-02-14 18:06:33 -0300

    add custom  page login

--- src/auth.ts
+++ src/auth.ts
@@ -1,29 +1,57 @@
 import NextAuth from "next-auth";
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
+import type { Provider } from "next-auth/providers"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
-      }
-      return true;
+
+const providers: Provider[] = [
+  MicrosoftEntraID({
+    clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+    clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+    issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+  }),
+]
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider()
+      return { id: providerData.id, name: providerData.name }
+    } else {
+      return { id: provider.id, name: provider.name }
+    }
+  })
+  .filter((provider) => provider.id !== "credentials")
+
+  export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers,
+    pages: {
+      signIn: "/login",
     },
-  },
-  providers: [
-    MicrosoftEntraID({
-      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
-      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
-      issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
-    }),
-  ],
-  trustHost: true,
-});
+  })
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   pages: {
+//     signIn: "/login",
+//   },
+//   callbacks: {
+//     authorized({ auth, request: { nextUrl } }) {
+//       const isLoggedIn = !!auth?.user;
+//       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+//       if (isOnDashboard) {
+//         if (isLoggedIn) return true;
+//         return false; // Redirect unauthenticated users to login page
+//       } else if (isLoggedIn) {
+//         return Response.redirect(new URL("/dashboard", nextUrl));
+//       }
+//       return true;
+//     },
+//   },
+//   providers: [
+//     MicrosoftEntraID({
+//       clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+//       clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+//       issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+//     }),
+//   ],
+//   trustHost: true,
+// });
