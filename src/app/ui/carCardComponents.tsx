aefdc0d9c0b77931def7b1e83cdabca810ca24ef
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-12-07 16:03:31 -0300

    fix(cars) update carCard

--- src/app/ui/carCardComponents.tsx
+++ src/app/ui/carCardComponents.tsx
@@ -7,7 +7,7 @@ import { longHaul } from "./fonts/fonts"
 
 export function CarCardRoot({ children, team ,year, alignment}: { children: ReactNode, team : String ,year : String, alignment : "left" | "right" }) {
   return (
-    <div className="flex gap-2">
+    <div className="flex mx-auto justify-center">
       {alignment == "left" ?
         <h1 className={`text-6xl my-2 text-center ${longHaul.className} md:self-center md:mr-4 `}>
           <div className="hidden md:inline">
@@ -18,7 +18,7 @@ export function CarCardRoot({ children, team ,year, alignment}: { children: Reac
         <></>
       }
       <div className="bg-transparent border border-gray-800 rounded-xl w-[80%] h-[20%] p-3 ">
-        <div className="flex flex-col md:flex-row md:gap-y-3 md:gap-x-5 p-2 rounded-md">
+        <div className={`flex flex-col md:flex-row md:gap-y-3 md:gap-x-5 p-2 rounded-md ${alignment == "right"? "justify-end" : "justify-start"}`}>
           <div className={`inline md:hidden text-center text-6xl ${longHaul.className}`}>
             <p>{team}</p>
             <p className="text-red-600 -mt-6">{year}</p>
@@ -45,7 +45,7 @@ export function CarCardRoot({ children, team ,year, alignment}: { children: Reac
 
 export function CarCardImage({ alt, src }: ImageProps) {
   return (
-    <div className="mx-auto md:mx-0">
+    <div className="flex">
       <Image src={src} width={500} height={281.25} alt={alt} className="block md:inline-block md:w-[500px] md:h-[281px] object-contain"/>
     </div>
   )
@@ -53,7 +53,7 @@ export function CarCardImage({ alt, src }: ImageProps) {
 
 export function CarCardText({ children }: { children: ReactNode}) {
   return (
-    <div className={`flex flex-row items-center justify-center p-4`}>
+    <div className={`flex flex-row items-center justify-center p-4 grow`}>
       {children}
     </div>
   )
@@ -61,7 +61,7 @@ export function CarCardText({ children }: { children: ReactNode}) {
 
 export function CarCardDescription({children, alignment} : {children: ReactNode, alignment : String}){
   return(
-    <div className={`w-fit text-balance`}>
+    <div className={`w-fit md:text-balance md:text-pretty text-wrap`}>
       {children}
     </div>
   )
